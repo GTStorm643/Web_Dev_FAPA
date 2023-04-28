bfd2345cbbfc78bb588a0c2dbd37e464409d1ae1
Gustavo Teixeira da Silva <95049587+GTStorm643@users.noreply.github.com>
2023-04-28 19:22:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const data = new Array()
 
 const form = document.getElementById('form');
+const cardsContainer = document.getElementById("cards-container");
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+let idCounter = 1;
+let savedCards = JSON.parse(localStorage.getItem("cards")) || [];
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
 
-    validateInputs();
-});
+//     validateInputs();
+// });
 
 const setError = (element, message) => {
     const inputControl = element.parentElement;
@@ -58,7 +61,7 @@ const validateInputs = () => {
     if (passwordValue === '') {
         setError(password, 'Password is required');
     } else if (passwordValue.length < 6) {
-        setError(password, 'Password must be at least 8 character.')
+        setError(password, 'Password must be at least 6 character.')
     } else {
         setSuccess(password);
     }
@@ -73,38 +76,90 @@ const validateInputs = () => {
 
 };
 
-console.log(data)
-data.forEach((contato)=>{incluirLinhaTabela(contato)})
+// console.log(data)
+// data.forEach((contato)=>{incluirLinhaTabela(contato)})
 
-function addRegistration() {
-    let contato = {
-        name: document.getElementById('username').value,
-        email: document.getElementById('email').value,
-        password: document.getElementById('password2').value
-    }
-    document.getElementById('username').value = ''
-    document.getElementById('email').value= ''
-    document.getElementById('password2').value = ''
-    data.push(contato)
-    incluirLinhaTabela(contato)
-    console.log(data)
+// function addRegistration() {
+//     debugger
+//     var valUser = document.getElementById('username').value = ''
+//     var valEmail = document.getElementById('email').value= ''
+//     var valPassW = document.getElementById('password2').value = ''
+//         let contato = {
+//             name: document.getElementById('username').value,
+//             email: document.getElementById('email').value,
+//             password: document.getElementById('password2').value
+//         }
+//         data.push(contato)
+//         incluirLinhaTabela(contato)
+//         console.log(data)
+// }
+
+
+// function incluirLinhaTabela(user) {
+
+//     const tr = document.createElement('tr');
+//     console.log(tr)
+//     const td1 = document.createElement('td')
+//     td1.innerText = user.name
+//     const td2 = document.createElement('td')
+//     td2.innerText = user.email
+//     const td3 = document.createElement('td')
+//     td3.innerText = user.password
+
+//     tr.appendChild(td1)
+//     tr.appendChild(td2)
+//     tr.appendChild(td3)
+//     console.log(tr)
+//     document.getElementById('bodyAgenda').appendChild(tr)
+//     td1.classList.add('bold')    
+// }
+
+
+function createCard(id, username, email, senha) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("id", `card-${id}`);
+  card.setAttribute("class", `card`);
+  card.innerHTML = `
+    <p><strong>Id:</strong> ${id}</p>
+    <p><strong>Nome:</strong> ${username}</p>
+    <p><strong>E-mail:</strong> ${email}</p>
+    <p><strong>Senha:</strong> ${senha}</p>
+  `;
+  return card;
 }
 
-function incluirLinhaTabela(user) {
-
-    const tr = document.createElement('tr');
-    console.log(tr)
-    const td1 = document.createElement('td')
-    td1.innerText = user.name
-    const td2 = document.createElement('td')
-    td2.innerText = user.email
-    const td3 = document.createElement('td')
-    td3.innerText = user.password
-
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    tr.appendChild(td3)
-    console.log(tr)
-    document.getElementById('bodyAgenda').appendChild(tr)
-    td1.classList.add('bold')    
+function renderCards() {
+  savedCards.forEach((cardData) => {
+    const card = createCard(
+      cardData.id,
+      cardData.username,
+      cardData.email,
+      cardData.senha,
+    );
+    cardsContainer.appendChild(card);
+    idCounter = cardData.id + 1;
+  });
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("password2").value;
+
+  const cardData = { id: idCounter, username, email, senha };
+  const card = createCard(idCounter, username, email, senha);
+
+  cardsContainer.appendChild(card);
+
+  idCounter++;
+
+  savedCards.push(cardData);
+  localStorage.setItem("cards", JSON.stringify(savedCards));
+
+  form.reset();
+});
+
+renderCards();
